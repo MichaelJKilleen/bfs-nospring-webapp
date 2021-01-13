9294ed3b3bf340e0ce20627ec6f95169c647484f
Michael Killeen <michaeljkilleen@outlook.com>
2021-01-12 22:05:47 -0500

    Initial

---
+++ src/main/java/com/bottomfeeder/controller/ViewController.java
@@ -0,0 +1,28 @@
+package com.bottomfeeder.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+    name = "viewController",
+    urlPatterns = {"/view/*"},
+    initParams = @WebInitParam(name = "renderer-class-name", value = "com.logicbig.HtmlRenderer"),
+    loadOnStartup = 1)
+public class ViewController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String renderer = getServletConfig().getInitParameter("renderer-class-name");
+        PrintWriter writer = resp.getWriter();
+        writer.println("renderer: " + renderer);
+
+        String servletName = getServletConfig().getServletName();
+        writer.println("servlet name " + servletName);
+    }
+}
